97519ac8791c53829ae75e3605e2dd8c199b5d1c
Artyom Khrebtov <t.khrebtov@yandex.ru>
2021-08-02 21:14:14 +0500

    Initial Commit

---
+++ lazyBartender/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lazyBartender
+//
+//  Created by Артем Хребтов on 02.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
